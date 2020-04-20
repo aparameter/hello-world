0addee4f33c1b843b3ba55f7c147c13b7934a3eb
Apramita <apramita.d@gmail.com>
2020-04-20 19:40:14 +0530

    New updates for Heroku Deployment

--- helloworld_project/helloworld_project/settings.py
+++ helloworld_project/helloworld_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p)l@is*nmk@du*ngg3$ei6*wzb%=qpn!i!s=8jw=o!!*48j)m0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
